987f3fbc7ff775fb12fbb2cc36200ecac444cf67
lesliejm01 <johnleslie01@gmail.com>
2016-01-05 11:28:52 -0600

    playing with file IO

--- csv_to_ldif.rb
+++ csv_to_ldif.rb
@@ -13,9 +13,21 @@ while !file.eof?
    puts line
 end
 
+datafile = File.open("sample.txt", "r")
+lines = datafile.readlines         
+datafile.close
+
+lines.each{ |line| puts line }   
+
 File.open("sample.txt").readlines.each do |line|
    puts line
 end
+
+lines = File.open("sample.txt", "r"){ |datafile| 
+   datafile.readlines
+}
+
+lines.each{|line| puts line}
 
 # Do/While
 
