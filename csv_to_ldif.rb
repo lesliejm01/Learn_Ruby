b9d6fdcb9e481425b894384fc1671f6306d009e2
lesliejm01 <johnleslie01@gmail.com>
2016-01-05 13:14:41 -0600

    Added a Method to handle File IO
    Cleaned up the script and added a Method to handle File IO as well as
    take in CLI ARGS

--- csv_to_ldif.rb
+++ csv_to_ldif.rb
@@ -1,40 +1,35 @@
 # CSV to LDIF script
 # Author: John Leslie
 # Date: 28 DEC 2015
-# 
+# Usage: # ruby script.rb <inputfile> <outputfile>
 
-# "Includes"
+# Method
+def PDPCONTEXT(input,output)
+  # pass ARGS to respective File operations
+  infile = File.open(input, 'r')
+  ldiffile = File.open(output, "w")
+  # Begin Looping through CSV file, line by line
+  while !infile.eof?
+     line = infile.readline
+     # put each line into an array where each element in the array is delimitated by a ','
+     csvline = line.split(",")
+     #print "#{csvline}\n"
+     # Set up the LDIF entry for each line in the CSV by accessing the array. each element in the array is a value in the entry
+     ldiffile.puts "dn: pdpContextName=PDP_<entry count>,o=pdpcontext,o=hlr,o=nss,o=services,o=default,DC=C-NTDB"
+     ldiffile.puts "vplmnAllowed: #{csvline[0]}"
+     ldiffile.puts "pdpType: #{csvline[1]}"
+     ldiffile.puts "accPointName: #{csvline[2]}"
+     ldiffile.puts "chargCharac: #{csvline[3]}"
+     ldiffile.puts "extPdpType: #{csvline[4]}"
+     ldiffile.puts "\n"
+  end # END OF WHILE LOOP
+  ldiffile.close # close output file
+  infile.close # THEN close input file
+end # END of Method
 
+# Take in ARGS
+input = ARGV[0] # input file
+output = ARGV[1] # output file
 
-# File I/O
-=begin
-file = File.open("sample.txt", 'r')
-while !file.eof?
-   line = file.readline
-   puts line
-end
-
-datafile = File.open("sample.txt", "r")
-lines = datafile.readlines         
-datafile.close
-
-lines.each{ |line| puts line }   
-
-File.open("sample.txt").readlines.each do |line|
-   puts line
-end
-
-lines = File.open("sample.txt", "r"){ |datafile| 
-   datafile.readlines
-}
-
-lines.each{|line| puts line}
-=end
-somefile = File.open("sample.txt", "w")
-somefile.puts "Hello file!"
-somefile.close
-# Do/While
-
-
-# Checks
-
+# Call Method
+PDPCONTEXT(input,output)
