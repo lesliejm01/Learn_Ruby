4ed44786a80c055b68501be14ec8c655046ceb6a
lesliejm01 <johnleslie01@gmail.com>
2016-01-08 12:14:58 -0600

    added ldif output file to 'ignore' list / added PDP_entry_count counter
    PDP_entry_count counter added and commented
    .gitignore added to include LDIF output file

--- csv_to_ldif.rb
+++ csv_to_ldif.rb
@@ -5,8 +5,9 @@
 
 # Method
 def PDPCONTEXT(input,output)
-  # Set up a "counter" and initialize to 0
-  entry_count = 0
+  # Set up a "counter" and initialize to 0, note: counter must be an 'Instance Variable' or error 'dynamic constant assignment' is thrown
+  # This is due to it the method dynamically assigning it everytime it is called, and must be "visible" outside the method.
+  @PDP_entry_count = 0
   # pass ARGS to respective File operations
   infile = File.open(input, 'r')
   ldiffile = File.open(output, "w")
@@ -15,11 +16,8 @@ def PDPCONTEXT(input,output)
      line = infile.readline
      # put each line into an array where each element in the array is delimitated by a ','
      csvline = line.split(",")
-     #print "#{csvline}\n"
-     # Generate RDN: PDP_<entry count>
-     PDP_entry_count = entry_count
      # Set up the LDIF entry for each line in the CSV by accessing the array. each element in the array is a value in the entry
-     ldiffile.puts "dn: pdpContextName=#{PDP_entry count},o=pdpcontext,o=hlr,o=nss,o=services,o=default,DC=C-NTDB"
+     ldiffile.puts "dn: pdpContextName=#{$PDP_entry_count},o=pdpcontext,o=hlr,o=nss,o=services,o=default,DC=C-NTDB"
      ldiffile.puts "objectClass: PDPCONTEXT"  # objectClass name for the entry
      ldiffile.puts "vplmnAllowed: #{csvline[0]}"
      ldiffile.puts "pdpType: #{csvline[1]}"
@@ -28,7 +26,7 @@ def PDPCONTEXT(input,output)
      ldiffile.puts "extPdpType: #{csvline[4]}"
      ldiffile.puts "\n"
      # Increment the counter
-     entry_count = entry_count + 1
+     @PDP_entry_count = @PDP_entry_count + 1
   end # END OF WHILE LOOP
   ldiffile.close # close output file
   infile.close # THEN close input file
@@ -38,10 +36,11 @@ end # END of Method
 input = ARGV[0] # input file
 output = ARGV[1] # output file
 
+# Basic Error Handling / 'help' statement
 if ARGV.length == 0 || ARGV[0] == "-help"
 	print "\nPlease use the following format:\n$ ruby csv_to_ldif.rb <input_file.csv> <output_file.ldif>\nRun: $ ruby csv_to_ldif.rb -help   to display this message.\n\n"
 else
-	# Call Method
+	# Call Method / Convert CSV --> LDIF
 	PDPCONTEXT(input,output)	
 end
 
