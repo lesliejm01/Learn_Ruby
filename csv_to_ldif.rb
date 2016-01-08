d8610f9722511d20d3b0649f91051dea63f609a8
lesliejm01 <johnleslie01@gmail.com>
2016-01-08 11:20:55 -0600

    added PDP_entry_count and objectClass to entry output
    PDP_entry_count: simply increments from 0 for each entry in CSV
    "objectClass: PDPCONTEXT" is added to each entry upon creation

--- csv_to_ldif.rb
+++ csv_to_ldif.rb
@@ -5,6 +5,8 @@
 
 # Method
 def PDPCONTEXT(input,output)
+  # Set up a "counter" and initialize to 0
+  entry_count = 0
   # pass ARGS to respective File operations
   infile = File.open(input, 'r')
   ldiffile = File.open(output, "w")
@@ -14,14 +16,19 @@ def PDPCONTEXT(input,output)
      # put each line into an array where each element in the array is delimitated by a ','
      csvline = line.split(",")
      #print "#{csvline}\n"
+     # Generate RDN: PDP_<entry count>
+     PDP_entry_count = entry_count
      # Set up the LDIF entry for each line in the CSV by accessing the array. each element in the array is a value in the entry
-     ldiffile.puts "dn: pdpContextName=PDP_<entry count>,o=pdpcontext,o=hlr,o=nss,o=services,o=default,DC=C-NTDB"
+     ldiffile.puts "dn: pdpContextName=#{PDP_entry count},o=pdpcontext,o=hlr,o=nss,o=services,o=default,DC=C-NTDB"
+     ldiffile.puts "objectClass: PDPCONTEXT"  # objectClass name for the entry
      ldiffile.puts "vplmnAllowed: #{csvline[0]}"
      ldiffile.puts "pdpType: #{csvline[1]}"
      ldiffile.puts "accPointName: #{csvline[2]}"
      ldiffile.puts "chargCharac: #{csvline[3]}"
      ldiffile.puts "extPdpType: #{csvline[4]}"
      ldiffile.puts "\n"
+     # Increment the counter
+     entry_count = entry_count + 1
   end # END OF WHILE LOOP
   ldiffile.close # close output file
   infile.close # THEN close input file
